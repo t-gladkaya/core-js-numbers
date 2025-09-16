5f1d1bbdf7f3f699f5909bb07d20a43d2f81117a
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-16 21:45:28 +0300

    feat: implement getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -255,8 +255,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= index; i += 1) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 }
 
 /**
