29d041963c65df5713a6f37faee669721445e89a
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-16 21:54:13 +0300

    feat: implement getSumToN function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -280,8 +280,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  const arr = [];
+  for (let i = 1; i <= n; i += 1) {
+    arr.push(i);
+  }
+  return arr.reduce((sum, el) => sum + el, 0);
 }
 
 /**
