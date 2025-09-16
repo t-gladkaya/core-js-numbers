7cdc75a298f6e482d0252f7bdae70914d403606c
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-16 16:54:17 +0300

    feat: implement getAngleBetweenVectors function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -104,8 +104,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const mult = x1 * x2 + y1 * y2;
+  const aLength = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const bLength = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cosTheta = mult / (aLength * bLength);
+  return Math.acos(cosTheta);
 }
 
 /**
