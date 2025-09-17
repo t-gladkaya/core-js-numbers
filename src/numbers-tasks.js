ea4eaf8b5459faa33b99d6448316e6e9910908f8
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-17 17:59:42 +0300

    feat: implement getSumOfNumbers function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -565,8 +565,9 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const sum = [x1, x2, x3].reduce((s, el) => s + el, 0);
+  return Number(sum.toFixed(10));
 }
 
 /**
